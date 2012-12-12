721e7636959480df66b921ba8b033f5912c210fa
Marax <marax@marax.cz>
2012-12-12 15:02:57 +0100

    test objektu

--- src/objects/Sp2.java
+++ src/objects/Sp2.java
@@ -6,7 +6,7 @@ public class Sp2 implements IObject {
 
 	@Override
 	public Vec3D compute(double x, double y) {
-		double s = Math.PI * 0.5 - Math.PI * y *2;
+	/*	double s = Math.PI * 0.5 - Math.PI * y *2;
 		double t = 2 * Math.PI * x *2; 
 		double r = 1+Math.sin(4*t);
 		r *= 8; 
@@ -15,6 +15,16 @@ public class Sp2 implements IObject {
 				Math.sin(t) * Math.cos(s) * r ,
 				Math.sin(t) * Math.sin(s) * r , 
 				Math.cos(s) * r );
+	*/
+		double s = Math.PI * y *2;
+		double t = Math.PI * x; 
+		double r = 1+Math.sin(4*t);
+		r *= 8; 
+		
+		return new Vec3D(
+				3+Math.cos(4*s) ,
+				t*Math.sin(s) , 
+				s );
 	}
 
 }
